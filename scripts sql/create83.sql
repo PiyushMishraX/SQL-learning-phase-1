38181bbf1523364f8771662d1f455ee8bb9058d6
PiyushMishraX <piyushmishra.connect@gmail.com>
2026-03-12 20:14:43 +0530

    Add files via upload

---
+++ scripts sql/create83.sql
@@ -0,0 +1,6 @@
+-- INSERT INTO users ( name, email, gender, date_of_birth, salary) VALUES
+-- ('bro83', 'bro83@example.com', 'Male', '1965-04-12', 10000);
+
+
+-- INSERT INTO admin_users (id,name, email, gender, date_of_birth, salary) VALUES
+-- (105, 'bro83', 'bro83@example.com', 'Male', '1965-04-12', 10000);
