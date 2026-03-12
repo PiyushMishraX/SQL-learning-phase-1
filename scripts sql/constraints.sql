274c196d57bc83ff6235853cfdfb1e4cf38c0266
PiyushMishraX <piyushmishra.connect@gmail.com>
2026-03-12 20:14:19 +0530

    Add files via upload

---
+++ scripts sql/constraints.sql
@@ -0,0 +1,19 @@
+USE startersql;
+
+
+-- INSERT INTO users (name, email, gender, date_of_birth, salary) VALUES
+-- ('Aarav', 'aarav@example.com', 'Male', '1995-05-14', 65000.00);
+
+-- ALTER TABLE users ADD CONSTRAINT unique_email UNIQUE(email);
+
+
+-- INSERT INTO users (name, email, gender, date_of_birth, salary) VALUES
+-- (NULL, 'aarav23@example.com', 'Male', '1995-05-14', 65000.00);
+
+-- ALTER TABLE users ADD CONSTRAINT check_dob CHECK (date_of_birth >'1920-01-01');
+-- INSERT INTO users (name, email, gender, date_of_birth, salary) VALUES
+-- ('bro', 'aarav23@example.com', 'Male', '1919-05-14', 65000.00);
+
+
+
+SELECT * FROM userS;
