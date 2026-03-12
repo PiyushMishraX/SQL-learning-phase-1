1866894391144eba7d15575b0370ed57c0a3e205
PiyushMishraX <piyushmishra.connect@gmail.com>
2026-03-12 20:14:07 +0530

    Add files via upload

---
+++ scripts sql/auto commit and transactions.sql
@@ -0,0 +1,16 @@
+use startersql;
+
+-- set autocommit=0;
+-- select * from users;
+-- COMMIT;
+
+ -- DELETE FROM users WHERE id=28;
+-- ROLLBACK;
+
+-- DELETE FROM users WHERE id=28;
+-- select * from users;
+-- commit;
+
+set autocommit=1;
+
+select * from users;
