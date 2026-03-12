a2c1bf7086c52245cf236ef7c7a7c54c1511a290
PiyushMishraX <piyushmishra.connect@gmail.com>
2026-03-12 20:14:13 +0530

    Add files via upload

---
+++ scripts sql/chatgpt trick.sql
@@ -0,0 +1,4 @@
+ -- upload you sql queries in chatgpt and ask for updation deletion scripts etc
+ 
+ -- change and ask for mysql to postgres scripts if you want to learn it or use it
+  -- check if mysql same script work on postgres and if not change it throw change gpt
