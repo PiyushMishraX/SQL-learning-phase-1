865bfe776fbc1aac6d1a97016bcd02cf279a1632
PiyushMishraX <piyushmishra.connect@gmail.com>
2026-03-12 20:15:01 +0530

    Add files via upload

---
+++ scripts sql/foeign key 3.sql
@@ -0,0 +1,12 @@
+use startersql;
+
+
+-- select * from users;
+-- select * from addresses;
+
+
+-- delete from addresses where id=2;
+
+delete from users where id=2;
+select * from users;
+select * from addresses;
