a542538e507c37c3461e02e3ef0e926a25e1ca2a
PiyushMishraX <piyushmishra.connect@gmail.com>
2026-03-13 22:30:16 +0530

    Add files via upload

---
+++ scripts sql/primary key.sql
@@ -0,0 +1,15 @@
+use startersql;
+
+-- primary key and unique - tabe can have multiple unique constraints but only one primary key
+-- create table testpk(
+-- 	id int auto_increment primary key,
+--     email varchar(100) unique,
+--     name varchar(100)
+-- );
+
+-- alter table testpk drop id;
+
+-- ALTER TABLE testpk AUTO_INCREMENT = 1000;
+
+
+select * from users;
