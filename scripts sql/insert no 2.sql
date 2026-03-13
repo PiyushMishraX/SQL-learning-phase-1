36f37c9ac0e6c2fd6686fde480bba154df61b09a
PiyushMishraX <piyushmishra.connect@gmail.com>
2026-03-13 22:27:38 +0530

    Add files via upload

---
+++ scripts sql/insert no 2.sql
@@ -0,0 +1,8 @@
+-- INSERT INTO users (name, email, gender, date_of_birth, salary) VALUES
+-- ('Ananya', 'ananya@example.com', 'Female', '1990-11-23', 72000.00);
+
+-- DELETE FROM users WHERE id=28;
+
+-- INSERT INTO users (id,name, email, gender, date_of_birth, salary) VALUES
+-- (2,'Ananya', 'ananya@example.com', 'Female', '1990-11-23', 72000.00);
+select * from users;
