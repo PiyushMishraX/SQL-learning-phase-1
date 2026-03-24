9c7a557db483609102d53a7db4a3c775d638111d
PiyushMishraX <piyushmishra.connect@gmail.com>
2026-03-24 21:59:47 +0530

    Add files via upload

---
+++ scripts sql/views.sql
@@ -0,0 +1,9 @@
+use startersql;
+
+-- create view rich_users as 
+-- select * from users where salary > 70000;
+
+-- update users set salary=30000 where id=2;
+select * from rich_users;
+
+drop view rich_users;
