9cc85acf65eca4c1ad00fdf4fe17962ab4f81882
PiyushMishraX <piyushmishra.connect@gmail.com>
2026-03-24 21:57:46 +0530

    Add files via upload

---
+++ scripts sql/tips.sql
@@ -0,0 +1,5 @@
+ -- practice take-resposibility ;everage-ai use-your-knowledge  
+ -- download handbook
+ -- LEETCODE SQL 50 , create project , practice , questions etc
+ 
+  -- practice sql training QUESTIONS from college 
